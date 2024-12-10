b54d6f2b08229bb859d98a77b62888178cdface3
niek-sidn <70195544+niek-sidn@users.noreply.github.com>
2024-12-10 15:01:17 +0100

    Small changes and better verification script (#18)

--- files/nsd-validator/verify.sh
+++ files/nsd-validator/verify.sh
@@ -1,14 +1,58 @@
 #!/usr/bin/env bash
-set -o errexit
-set -o pipefail
-## Some verifiers cannot use stdin directly, and need an actual file to verify
-## kzonesign -v is such a verifier, but it is too good to ignore (verifies at 1200% CPU in stead of 100%!)
-## To accomodate kzonesign we direct the stdin feed from nsd to the path dictated by knot.conf
-## (this is the actual knot.conf used by knot-signer shared in by volume)
-ZF=/var/lib/knot/zones/$VERIFY_ZONE.zone   # path is dictated by /etc/knot/knot.conf, VERIFY_ZONE provided by NSD
-cat > $ZF                                  # read from stdin (nsd: verifier-feed-zone: yes) to file (in memory if not on volume)
-kzonesign -v $VERIFY_ZONE                  # will use 'policy' and 'signing-threads'(!) and 'file' settings from /etc/knot/knot.conf
-## Others can use it too
-nsd-checkzone $VERIFY_ZONE $ZF
-##
-named-checkzone -i local-sibling $VERIFY_ZONE $ZF
+set -o errexit  # if commands fail, quit
+set -o pipefail # if anything in a pipe construction fails, quit
+
+## Some verifiers cannot use stdin from 'NSD verifier-feed-zone: yes' directly, and need an
+## actual file to verify. So we direct the stdin feed from NSD to a file.
+## The path may surprise you, so keep reading.
+ZF=/var/lib/knot/zones/$VERIFY_ZONE.zone
+echo "writing $VERIFY_ZONE to file $ZF"
+cat > $ZF                                  # read from stdin to file (in memory if not on volume)
+echo "writing $VERIFY_ZONE to file $ZF finished"
+
+## start the verifing software in parallel by backgrounding, please
+## note the waiting loop below the starting of the verifing software.
+echo 'Starting nsd-checkzone'
+nsd-checkzone $VERIFY_ZONE $ZF &
+pid=$!
+checkzone1_pid="${pid}"
+#
+echo 'Starting named-checkzone'
+named-checkzone -i none $VERIFY_ZONE $ZF &   # '-i none': no post zone load checks
+pid=$!
+checkzone2_pid="${pid}"
+#
+## kzonesign -v is a great multithreaded verifier, but it takes some work:
+## a) it cannot take the zonefile on stdin (that is taken care of above with 'cat >').
+## b) it takes its config from a configfile. By default /etc/knot/knot.conf.
+## To solve b) and avoid maintaining two knot.conf files, this nsd-validator
+## gets the official knot.conf from knot-signer, by volume in compose.yml.
+## This way it gets the correct dnssec policy, but also the same number of
+## signing-threads as knot-signer (which you may like or not) and also the
+## same path to the zonefile as knot-signer (which is a bit awkward). Hence the
+## surprising /var/lib/knot/zones/... path above.
+echo 'Starting kzonesign -v'
+kzonesign -v $VERIFY_ZONE &
+pid=$!
+kzonesign_pid="${pid}"
+#
+## Validns is is a great multithreaded verifier.
+echo 'Starting validns'
+validns -n 8 -s $ZF &
+pid=$!
+validns_pid="${pid}"
+#
+## Now wait for all backgrounded processes to finish
+for p in kzonesign_pid checkzone1_pid checkzone2_pid validns_pid
+do
+    if wait "${!p}"; then
+        echo "Process ${p%_*} success"
+    else
+        # there were one or more failed processes.
+	# Note: errexit and/or pipefail may have already terminated this script,
+	#       so don't assume that not seeing the next output implies success.
+        echo "ERROR: checking signed zonefile using ${p} failed"
+        exit 1
+    fi
+done
+echo "$0 done"
