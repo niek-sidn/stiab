77dd565d35d9d7f56b83295fb51f3e252c443294
Niek Willems <niek.willems@sidn.nl>
2024-12-16 09:48:07 +0100

    Added exec to entrypoint daemon processes, dns-client can be safely sigkilled

--- entrypoints/entrypoint_dnsclient.sh
+++ entrypoints/entrypoint_dnsclient.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sleep infinity
+exec sleep infinity
--- entrypoints/entrypoint_knotd.sh
+++ entrypoints/entrypoint_knotd.sh
@@ -3,4 +3,4 @@ if [ "$HOSTNAME" != knot-fakeroot ] && [ "$HOSTNAME" != knot-secondlevel ]; then
    rm /var/lib/knot/zones/* /var/lib/knot/journal/* > /dev/null 2>&1
 fi
 chown --recursive knot:knot /run/knot/ /var/lib/knot /etc/knot
-knotd
+exec knotd
